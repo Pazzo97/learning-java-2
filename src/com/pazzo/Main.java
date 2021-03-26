65e96905c24ef898b1a99f27b6f3a9149a963805
Patrick <pazzomuk@gmail.com>
2021-03-26 19:21:44 +0300

    Add getAverage method

--- src/com/pazzo/Main.java
+++ src/com/pazzo/Main.java
@@ -10,11 +10,12 @@ public class Main {
 
         int[] myIntegers= getIntegers(5);
 
-        for (int i=0;i< myIntegers.length;i++){
+        for (int i=0; i < myIntegers.length; i++){
 
             System.out.println("Element "+ i +" typed was "+ myIntegers[i]);
-
         }
+
+        System.out.println("\nThe average is " + getAverage(myIntegers));
 
     }
 
@@ -28,6 +29,18 @@ public class Main {
             values[i]= scanner.nextInt();
         }
         return values;
+
+    }
+
+    public static double getAverage (int[] array){
+
+        int sum =0;
+
+        for (int i=0;i< array.length;i++){
+
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
 
     }
 
